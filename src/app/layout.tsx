f2e2bf0ccaba66d66bcb92bbd3b5c676bbe18787
ddhsl <dnjs9199@naver.com>
2025-12-15 21:46:14 +0900

    style: add bg main style to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
-      <body className="bg-background text-secondary font-sans">{children}</body>
+      <body className="bg-main text-secondary font-sans">{children}</body>
     </html>
   );
 }
