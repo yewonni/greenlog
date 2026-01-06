d17e9d7133e1e6f81e6df84002469d2bce47457f
ddhsl <dnjs9199@naver.com>
2026-01-06 22:08:05 +0900

    feat: add login schema

---
+++ src/schemas/auth/login.schema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z
+    .string()
+    .min(1, "이메일을 입력해주세요.")
+    .email("올바른 이메일 형식이 아닙니다."),
+
+  password: z.string().min(1, "비밀번호를 입력해주세요."),
+});
